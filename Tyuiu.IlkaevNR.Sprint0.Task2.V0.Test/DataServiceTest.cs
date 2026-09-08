a5cfd655904ed600027f7cc9064ea9cc8ae112bf
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-09 01:30:24 +0500

    Task2

---
+++ Tyuiu.IlkaevNR.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.IlkaevNR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Наиль";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Наиль", res);
+        }
+    }
+}
---
+++ Tyuiu.IlkaevNR.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.IlkaevNR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Наиль"));
+            Console.ReadKey();
+        }
+    }
+}
