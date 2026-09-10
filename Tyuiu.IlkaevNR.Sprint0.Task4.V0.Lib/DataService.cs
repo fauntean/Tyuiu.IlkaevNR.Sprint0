22184fae1c13e125bb0c036b2c81565f95871dd5
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-10 18:39:29 +0500

    Task4

---
+++ Tyuiu.IlkaevNR.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿namespace Tyuiu.IlkaevNR.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {  
+            return a + b; 
+        }
+
+        public static int Substraction(int a, int b)
+        { 
+            return a - b; 
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
