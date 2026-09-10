5bc63be3f5b904ecd45751c1cf64e6c402fc3615
Ilkaev Nail <ilkaevnail3@gmail.com>
2026-09-10 18:20:00 +0500

    Task3

---
+++ Tyuiu.IlkaevNR.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.IlkaevNR.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.IlkaevNR.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7,7));
+            Console.ReadKey();
+        }
+    }
+}
